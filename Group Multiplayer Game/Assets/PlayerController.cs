594d411f6f02f7e2ea5380416a004b8741c0f5e7
AndrewSullivan <andrewsullivan121@outlook.com>
2021-11-01 17:04:51 +0000

    Implemented PhotonView.IsMine Function to Allow for Each Player to Control Their Own Character

--- Group Multiplayer Game/Assets/PlayerController.cs
+++ Group Multiplayer Game/Assets/PlayerController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class PlayerController : MonoBehaviour
+using Photon.Pun;
+
+public class PlayerController : MonoBehaviourPun
 {
     public float turnSpeed = 180;
     public float tiltSpeed = 180;
@@ -19,12 +21,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float forward = Input.GetAxis("Vertical");
-        float turn = Input.GetAxis("Horizontal") + Input.GetAxis("Mouse X");
-        float tilt = Input.GetAxis("Mouse Y");
-        transform.Translate(new Vector3(0, 0, forward * walkSpeed * Time.deltaTime));
-        transform.Rotate(new Vector3(0, turn * turnSpeed * Time.deltaTime, 0));
-        if (fpcam != null)
-            fpcam.Rotate(new Vector3(-tilt * tiltSpeed * Time.deltaTime, 0));
+        if (photonView.IsMine)
+        {
+            float forward = Input.GetAxis("Vertical");
+            float turn = Input.GetAxis("Horizontal") + Input.GetAxis("Mouse X");
+            float tilt = Input.GetAxis("Mouse Y");
+            transform.Translate(new Vector3(0, 0, forward * walkSpeed * Time.deltaTime));
+            transform.Rotate(new Vector3(0, turn * turnSpeed * Time.deltaTime, 0));
+            if (fpcam != null)
+                fpcam.Rotate(new Vector3(-tilt * tiltSpeed * Time.deltaTime, 0));
+        }
+        
     }
 }
