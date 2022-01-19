fc02e70a266cd06352cf58fba58334a45f390594
AndrewSullivan <andrewsullivan121@outlook.com>
2022-01-19 15:57:53 +0000

    Changed Killer & Survivor Controller Scripts and Changed Killer & Survivor Animations

--- Group Multiplayer Game/Assets/Resources/Scripts/KillerController.cs
+++ Group Multiplayer Game/Assets/Resources/Scripts/KillerController.cs
@@ -1,65 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Photon.Pun;
 
 public class KillerController : MonoBehaviourPun
 {
     public float walkSpeed = 10;
 
-    public float mouseSens = 300f;
+    Vector2 killerRotation;
 
-    public float rotClamp = 90f;
-
-    float rotationX;
-
-    float rotationY;
+    public Transform killer;
 
     public CharacterController killerController;
 
     public Animator killerAnim;
+
+    Vector3 killerMovement;
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         killerAnim = GetComponent<Animator>();
-
-        Vector3 mouseRotation = transform.localRotation.eulerAngles;
-
-        rotationX = mouseRotation.x;
-        rotationY = mouseRotation.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         if (photonView.IsMine)
         {
             // Player Input
             float killerZ = Input.GetAxisRaw("Vertical");
-            float killerX = Input.GetAxisRaw("Horizontal");
 
             // Player Movement
-            Vector3 killerMovement = new Vector3(killerX, 0, killerZ).normalized;
+            killerMovement = killerController.transform.forward * killerZ;
 
             killerController.Move(killerMovement * walkSpeed * Time.deltaTime);
 
-            killerAnim.SetFloat("KillerHorizontal", Input.GetAxisRaw("Horizontal"), 0.08f, Time.deltaTime);
-            killerAnim.SetFloat("KillerVertical", Input.GetAxisRaw("Vertical"), 0.08f, Time.deltaTime);
+            // Mouse Control
+            killerRotation.x += Input.GetAxis("Mouse X");
 
-            // Mouse Input
-            float lookX = Input.GetAxis("Mouse X");
-            float lookY = Input.GetAxis("Mouse Y");
+            killer.localRotation = Quaternion.Euler(0f, killerRotation.x, 0f);
 
-            rotationX -= lookY * mouseSens * Time.deltaTime;
-            rotationY += lookX * mouseSens * Time.deltaTime;
-
-            rotationX = Mathf.Clamp(rotationX, -rotClamp, rotClamp);
-
-            Quaternion killerRotation = Quaternion.Euler(rotationX, rotationY, 0f);
-            transform.rotation = killerRotation;
+            // Animations
+            killerAnim.SetFloat("KillerVertical", Input.GetAxis("Vertical"), 0.05f, Time.deltaTime);
         }
         
     }
--- Group Multiplayer Game/Assets/Resources/Scripts/SurvivorController.cs
+++ Group Multiplayer Game/Assets/Resources/Scripts/SurvivorController.cs
@@ -1,65 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using Photon.Pun;
 
 public class SurvivorController : MonoBehaviourPun
 {
     public float walkSpeed = 10;
 
-    public float mouseSens = 300f;
+    Vector2 survivorRotation;
 
-    public float rotClamp = 90f;
-
-    float rotationX;
-
-    float rotationY;
+    public Transform survivor;
 
     public CharacterController survivorController;
 
     public Animator survivorAnim;
+
+    Vector3 survivorMovement;
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         survivorAnim = GetComponent<Animator>();
-
-        Vector3 mouseRotation = transform.localRotation.eulerAngles;
-
-        rotationX = mouseRotation.x;
-        rotationY = mouseRotation.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         if (photonView.IsMine)
         {
             // Player Input
             float survivorZ = Input.GetAxisRaw("Vertical");
-            float survivorX = Input.GetAxisRaw("Horizontal");
 
             // Player Movement
-            Vector3 survivorMovement = new Vector3(survivorX, 0, survivorZ).normalized;
+            survivorMovement = survivorController.transform.forward * survivorZ;
 
             survivorController.Move(survivorMovement * walkSpeed * Time.deltaTime);
 
-            survivorAnim.SetFloat("SurvivorHorizontal", Input.GetAxisRaw("Horizontal"), 0.08f, Time.deltaTime);
-            survivorAnim.SetFloat("SurvivorVertical", Input.GetAxisRaw("Vertical"), 0.08f, Time.deltaTime);
+            // Mouse Control
+            survivorRotation.x += Input.GetAxis("Mouse X");
 
-            // Mouse Input
-            float lookX = Input.GetAxis("Mouse X");
-            float lookY = Input.GetAxis("Mouse Y");
+            survivor.localRotation = Quaternion.Euler(0f, survivorRotation.x, 0f);
 
-            rotationX -= lookY * mouseSens * Time.deltaTime;
-            rotationY += lookX * mouseSens * Time.deltaTime;
-
-            rotationX = Mathf.Clamp(rotationX, -rotClamp, rotClamp);
-
-            Quaternion survivorRotation = Quaternion.Euler(rotationX, rotationY, 0f);
-            transform.rotation = survivorRotation;
+            // Animations
+            survivorAnim.SetFloat("SurvivorVertical", Input.GetAxis("Vertical"), 0.05f, Time.deltaTime);
         }
         
     }
